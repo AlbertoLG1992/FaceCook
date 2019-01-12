1a70d70c173567daa54f7ce43db9e2513f018c96
AlbertoLG1992 <albertolg1992@gmail.com>
2019-01-12 16:05:14 +0100

    Main activity creada
    Añadida MainActivity. Tambien se ha modificado el estilo y los colores de la app

--- app/src/main/java/com/example/alberto/facecook/MainActivity.java
+++ app/src/main/java/com/example/alberto/facecook/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.alberto.facecook;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /* Elementos */
+    Toolbar toolbar;
+    Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.iniciarElementos();
+        this.iniciarToolbar();
+    }
+
+    /**
+     * Inicia y enlaza los elementos del XML
+     */
+    private void iniciarElementos(){
+        /* XML */
+        this.toolbar = (Toolbar)findViewById(R.id.toolbar);
+        this.btnEntrar = (Button)findViewById(R.id.btnEntrar);
+
+        /* Clicklable */
+        this.btnEntrar.setOnClickListener(this);
+    }
+
+    /**
+     * Carga el toolbar en la actividad
+     */
+    private void iniciarToolbar(){
+        this.toolbar.setTitle(R.string.app_name);
+        this.toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(this.toolbar);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //TODO ACTIVITY PARA SELECCIONAR SI QUEREMOS VER LOS COCINEROS O VER LAS RECETAS
     }
 }
