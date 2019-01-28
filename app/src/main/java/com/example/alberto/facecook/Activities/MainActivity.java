cd8a507174ff75320e92e7aec1f035f7dc18da2e
AlbertoLG1992 <albertolg1992@gmail.com>
2019-01-28 04:18:03 +0100

    Finalizado el xml en MainActivity
    Se a añadido la libreria de lottie para poder mostrar animaciones en JSON

--- app/src/main/java/com/example/alberto/facecook/Activities/MainActivity.java
+++ app/src/main/java/com/example/alberto/facecook/Activities/MainActivity.java
@@ -2,20 +2,22 @@ package com.example.alberto.facecook.Activities;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.alberto.facecook.Activities.ChoiceActivity;
 import com.example.alberto.facecook.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     /* Elementos */
-    Toolbar toolbar;
     Button btnEntrar;
+    LottieAnimationView lottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         this.iniciarElementos();
-        this.iniciarToolbar();
+        this.esconderStaturBar();
     }
 
     /**
@@ -31,25 +33,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void iniciarElementos(){
         /* XML */
-        this.toolbar = (Toolbar)findViewById(R.id.toolbarMain);
         this.btnEntrar = (Button)findViewById(R.id.btnEntrar);
+        this.lottieAnimationView = (LottieAnimationView)findViewById(R.id.lottieViewMain);
 
         /* Clicklable */
         this.btnEntrar.setOnClickListener(this);
-    }
-
-    /**
-     * Carga el toolbar en la actividad
-     */
-    private void iniciarToolbar(){
-        this.toolbar.setTitle(R.string.app_name);
-        this.toolbar.setTitleTextColor(Color.WHITE);
-        setSupportActionBar(this.toolbar);
+        this.lottieAnimationView.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(getApplicationContext(), ChoiceActivity.class);
-        startActivity(intent);
+        switch (v.getId()){
+            case R.id.btnEntrar:{
+                Intent intent = new Intent(getApplicationContext(), ChoiceActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.lottieViewMain:{
+                // TODO CAMBIAR LA IMAGEN
+                break;
+            }
+        }
+    }
+
+    /**
+     * Esconde el clor del statur bar
+     */
+    private void esconderStaturBar(){
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 }
