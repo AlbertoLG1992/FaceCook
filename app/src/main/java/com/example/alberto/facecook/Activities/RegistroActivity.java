8f8e1c1ac34e87250ed40b759ecc35888ac03922
AlbertoLG1992 <albertolg1992@gmail.com>
2019-02-07 14:16:20 +0100

    Finalizado el XML de RegistroActivity

--- app/src/main/java/com/example/alberto/facecook/Activities/RegistroActivity.java
+++ app/src/main/java/com/example/alberto/facecook/Activities/RegistroActivity.java
@@ -1,12 +1,14 @@
 package com.example.alberto.facecook.Activities;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
-import android.widget.TextView;
 
 import com.example.alberto.facecook.R;
 
@@ -15,7 +17,15 @@ public class RegistroActivity extends AppCompatActivity implements View.OnClickL
 
     /* Elementos */
     ImageView imgUser;
-    TextView txvInfoReg;
+    Toolbar toolbar;
+    TextInputLayout txilNick,
+                    txilPass,
+                    txilNombre,
+                    txilApellidos,
+                    txilFecha,
+                    txilTlf,
+                    txilComentarios;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +33,59 @@ public class RegistroActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_registro);
 
         this.iniciarElementos();
+        this.iniciarToolbar();
     }
 
     private void iniciarElementos(){
         /* XML */
         this.imgUser = (ImageView)findViewById(R.id.imgUser);
-        this.txvInfoReg = (TextView)findViewById(R.id.txvInfoReg);
+        this.toolbar = (Toolbar)findViewById(R.id.toolbarRegistro);
+        this.txilNick = (TextInputLayout)findViewById(R.id.txilNick);
+        this.txilPass = (TextInputLayout)findViewById(R.id.txilPass);
+        this.txilNombre = (TextInputLayout)findViewById(R.id.txilNombre);
+        this.txilApellidos = (TextInputLayout)findViewById(R.id.txilApellidos);
+        this.txilFecha = (TextInputLayout)findViewById(R.id.txilFecha);
+        this.txilTlf = (TextInputLayout)findViewById(R.id.txilTlf);
+        this.txilComentarios = (TextInputLayout)findViewById(R.id.txilComentarios);
 
         /* Clickable */
         this.imgUser.setOnClickListener(this);
+    }
+
+    private void iniciarToolbar() {
+        this.toolbar.setTitle("Registro");
+        setSupportActionBar(this.toolbar);
+    }
+
+    /**
+     * Método que carga en el toolbar un menú
+     *
+     * @param menu :Menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_add_receta, menu);
+        return true;
+    }
+
+    /**
+     * Método que se activa al pulsar sobre un item del menú
+     *
+     * @param item :MenuItem
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.itemGuardar:{
+                break;
+            }
+            case R.id.itemDescartar:{
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -49,7 +103,6 @@ public class RegistroActivity extends AppCompatActivity implements View.OnClickL
 
     @Override
     public boolean onMenuItemClick(MenuItem item) {
-        this.txvInfoReg.setVisibility(View.GONE);
         return false;
     }
 }
