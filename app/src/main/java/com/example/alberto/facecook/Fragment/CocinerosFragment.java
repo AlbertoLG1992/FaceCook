bcfc6add5e669d25811d6a282471cacd98b89491
AlbertoLG1992 <albertolg1992@gmail.com>
2019-02-21 13:44:00 +0100

    Añadido un mapFragment.

--- app/src/main/java/com/example/alberto/facecook/Fragment/CocinerosFragment.java
+++ app/src/main/java/com/example/alberto/facecook/Fragment/CocinerosFragment.java
@@ -9,24 +9,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.alberto.facecook.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CocinerosFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link CocinerosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class CocinerosFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class CocinerosFragment extends Fragment implements OnMapReadyCallback {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private MapView mapView;
+    private GoogleMap GMap;
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,45 +28,21 @@ public class CocinerosFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CocinerosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CocinerosFragment newInstance(String param1, String param2) {
-        CocinerosFragment fragment = new CocinerosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cocineros, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_cocineros, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteractionCocineros(uri);
-        }
+        /* Carga el fragment del mapa */
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        return view;
     }
 
     @Override
@@ -86,10 +56,23 @@ public class CocinerosFragment extends Fragment {
         }
     }
 
+    public void iniciarMapa(){
+    }
+
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        GMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        GMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        GMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     /**
